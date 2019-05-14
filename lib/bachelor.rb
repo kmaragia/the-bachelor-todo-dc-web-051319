3ab75dd6890a1158dcc61598f82d19a73cf1947a
Kemunto Maragia <kmaragia@email.wm.edu>
2019-05-14 03:22:42 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,16 +1,5 @@
 def get_first_name_of_season_winner(data, season)
-  data.each do |key,value|
-    if key == season.to_sym
-      value.each do |one,two|
-        if two == :"Winner"
-          a = data[key][one][:name]
-          x = a.split(" ")
-          t = x[0]
-        end
-      end
-    end
-  end
-  t
+  
 end
 
 def get_contestant_name(data, occupation)
